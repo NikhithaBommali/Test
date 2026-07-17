6bd58a885c6581e4b41fec793c7304794250cbe4
Nikhitha Bommali <nikhithabommali@Nikhithas-MacBook-Air.local>
2026-07-17 14:14:36 +0530

    Initial commit

---
+++ src/app/components/CaseStudies.js
@@ -0,0 +1,282 @@
+import React from 'react'
+import Image from 'next/image';
+import CallMadeIcon from '@mui/icons-material/CallMade';
+import Link from 'next/link';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import "aos/dist/aos.css";
+import inpharmd_ai from '../../../public/inpharmd_ai.png'
+import aws_logo from '../../../public/aws_logo.png'
+import drugs_ai from '../../../public/drugs_ai.png'
+import pinecone from '../../../public/pinecone.png'
+import X_logo from '../../../public/X_logo.png'
+import name_img from '../../../public/name.png'
+import pat_harper from '../../../public/pat_harper.png'
+import ashish from '../../../public/ashish.png'
+import paul_brobson from '../../../public/paul_brobson.png'
+
+
+const CaseStudies = () => {
+
+  return (
+    <div className="bg-gradient-to-r from-black-500 to-gray-600 w-full mt-[4rem] flex flex-col">
+
+      <div className='flex w-full flex-col items-center justify-center'>
+
+        <div className="text-[28px] text-white font-bold font-pockota m-[2rem] sm:m-[4rem]" data-aos="fade-in">
+          Case Studies & Blogs
+        </div>
+
+        {/* 4 cards */}
+        <div className='flex items-center justify-center flex-col'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-[2rem] p-4 text-white justify-center items-center'>
+
+            {/* card 1 */}
+            <div className='flex flex-col h-[24rem] w-[300px] lg:w-[380px] lg:h-[444px] 
+            p-[1rem] rounded-2xl justify-center sm:mb-[3rem] bg-white'
+              data-aos="fade-up">
+
+              <Image src={inpharmd_ai} alt='inpharamd_ai_img' className='rounded-3xl px-[10px] h-[200px]' />
+
+              <div className=' p-[0.5rem] rounded-xl mx-1 '>
+                <div className='text-[16px] text-black font-semibold font-sf_pro_display'>
+                  Decoding InpharmD - Revolutionizing Drug Information with AI
+                </div>
+                <div className='text-[12px] text-justify text-black'>I developed cutting-edge AI architecture with patented technology that seamlessly integrates with our LLM, housing 40 billion parameters with 95% accuracy for our specific needs.
+                </div>
+              </div>
+
+              <div className=''>
+                <a href='https://inpharmd.com/blogs/decoding-inpharmd-worlds-revolutionizing-drug-information-with-ai' target='_blank'>
+                  <button className='h-[2.5rem] px-4 py-2 mt-2 ml-1 font-sf_pro_display text-white rounded-full bg-black flex '>
+                    Read Case Study
+                    <span className='text-white p-1 '>
+                      <CallMadeIcon className='h-5 w-5 relative bottom-1' />
+                    </span>
+                  </button>
+                </a>
+              </div>
+
+            </div>
+
+            {/* card 2 */}
+            <div className='flex flex-col w-[300px] h-[24rem] lg:w-[380px] lg:h-[444px] 
+            p-[1rem]  rounded-2xl justify-center sm:mb-[3rem] bg-white'
+              data-aos="fade-up">
+
+              <Image src={aws_logo} alt='aws img' className='rounded-3xl px-[10px] h-[200px]' />
+
+              <div className=' p-[0.5rem] rounded-xl mx-1 '>
+                <div className='text-[16px] text-black flex font-semibold font-sf_pro_display'>
+                  AWS&apos;s Best AI Architecture in Machine Learning Blog</div>
+                <div className='text-[12px] text-justify text-black'>AWS has been essential in accelerating the development of Sherlock. We don&apos;t have to worry as much about scaling, systems maintenance, and stability because AWS takes care of it for us. ........</div>
+              </div>
+
+              <div className='items-start'>
+                <a href='https://aws.amazon.com/blogs/machine-learning/how-inpharmd-uses-amazon-kendra-and-amazon-lex-to-drive-evidence-based-patient-care' target='_blank'>
+                  <button className='h-[2.5rem] px-4 py-2 mt-2 ml-1 font-sf_pro_display text-white rounded-full bg-black flex '>
+                    Read Case Study
+                    <span className='text-white p-1 '>
+                      <CallMadeIcon className='h-5 w-5 relative bottom-1' />
+                    </span>
+                  </button>
+                </a>
+              </div>
+            </div>
+
+            {/* card 3 */}
+            <div className='flex flex-col w-[300px] h-[24rem] lg:w-[380px] lg:h-[444px] 
+            p-[1rem] rounded-2xl justify-center sm:mb-[3rem] bg-white'
+              data-aos="fade-up">
+
+              <Image src={drugs_ai} alt='drugs_ai_img' className='rounded-3xl px-[10px] h-[200px]' />
+
+              <div className=' p-[0.5rem] rounded-xl mx-1 '>
+                <div className='text-[16px] text-black font-semibold font-sf_pro_display'>
+                  Out of Pocket blog by Nikhil Krishnan
+                </div>
+                <div className='text-[12px] text-justify text-black'>Nikhil Krishnan, a creator of the healthcare newsletter &quot;Out of Pocket,&quot; with over 40K healthcare provider subscribers, featured our AI and product. </div>
+              </div>
+
+              <div className='items-start'>
+                <a href='https://www.outofpocket.health/p/clinical-pharmacists-generative-ai-and-inpharmd' target='_blank'>
+                  <button className='h-[2.5rem] px-4 py-2 mt-2 ml-1 font-sf_pro_display text-white rounded-full bg-black flex '>
+                    Read Case Study
+                    <span className='text-white p-1 '>
+                      <CallMadeIcon className='h-5 w-5 relative bottom-1' />
+                    </span>
+                  </button>
+                </a>
+              </div>
+            </div>
+
+            {/* card 4 */}
+            <div className='flex flex-col w-[300px] lg:w-[380px] lg:h-[444px] 
+            p-[1rem] h-[24rem] rounded-2xl justify-center sm:mb-[3rem] bg-white'
+              data-aos="fade-up">
+
+              <Image src={pinecone} alt='pinecone_img' className='rounded-3xl px-[10px] h-[200px]' />
+
+              <div className=' p-[0.5rem] rounded-xl mx-1 '>
+                <div className='text-[16px] text-black font-semibold font-sf_pro_display'>
+                  Pinecone Case Study
+                </div>
+                <div className='text-[12px] text-justify text-black'>Pinecone, the world&apos;s largest managed vector database and memory for AI products, appreciated my architecture and featured it on their blog with over 1 million views.
+                </div>
+              </div>
+
+              <div className=''>
+                <a href='https://www.pinecone.io/customers/inpharmd/' target='_blank'>
+                  <button className='h-[2.5rem] px-4 py-2 mt-2 ml-1 font-sf_pro_display text-white rounded-full bg-black flex '>
+                    Read Case Study
+                    <span className='text-white p-1 '>
+                      <CallMadeIcon className='h-5 w-5 relative bottom-1' />
+                    </span>
+                  </button>
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-8 flex items-center w-full justify-center flex-col'>
+
+        <div className='grid grid-rows-2 w-[336px] xl:w-[1170px] p-[1rem]'>
+          <div className="text-[12px] font-medium text-[rgb(81,124,101)] text-center"
+            data-aos="fade-in">
+            TESTIMONIAL
+          </div>
+          <div className="text-[28px] text-white text-center font-pockota font-bold"
+            data-aos="fade-in">
+            People Loves My Work
+          </div>
+        </div>
+
+        {/* 3 cards */}
+        <div className='grid grid-cols-1 lg:grid-cols-3 w-[336px] lg:w-[1130px] gap-4 p-4 md:p-0 m-6 md:mx-6 md:mb-[5rem] md:mt-6 text-white '>
+
+          <div className='flex flex-col h-[27rem] w-full p-4 rounded-2xl justify-center  bg-gray-800 border border-white border-opacity-20 text-white'
+            data-aos="fade-up">
+            <div className=' p-[0.5rem] text-justify rounded-xl text-[18px] flex items-center mt-6'>
+              &quot;Tulasee Rao Chintha is an exceptional AI specialist whose innovative work and visionary leadership have made a significant
+              impact in the tech industry. His ability to translate complex AI concepts into groundbreaking solutions is remarkable&quot;
+            </div>
+
+            <div className='mb-2 px-2 py-2 mt-4 text-white rounded-xl flex items-center'>
+              <Image src={ashish} alt='inpharamd ai img' className='rounded-full m-2 w-[70px] h-[70px]' />
+              <div className='flex flex-col ml-2 text-left'>
+                <span className='font-sf_pro_display text-[16px]'>Ashish Advani</span>
+                <span className='font-sf_pro_display text-[20px]'>Founder, InpharmD</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='flex flex-col h-[27rem] w-full p-4 rounded-2xl justify-center  bg-gray-800 border border-white border-opacity-20 text-white'
+            data-aos="fade-up">
+            <div className=' p-[0.5rem] text-justify rounded-xl text-[18px] flex items-center mt-6'>
+              &quot;Tulasee has been part of my organization for several years and has shown himself to be an exceptional contributor to our development organization.
+              He has a superior command of his subject matter, works well with his team and always brings creative solutions to bear&quot;
+            </div>
+
+            <div className='mb-2 px-2 py-2 mt-4 text-white rounded-xl flex items-center'>
+              <Image src={pat_harper} alt='inpharamd ai img' className='rounded-full m-2 w-[70px] h-[70px]' />
+              <div className='flex flex-col ml-2 text-left'>
+                <span className='font-sf_pro_display text-[16px]'>Pat Harper</span>
+                <span className='font-sf_pro_display text-[20px]'>CTO, Catalis</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='flex flex-col h-[27rem] w-full p-4 rounded-2xl justify-center  bg-gray-800 border border-white border-opacity-20 text-white'
+            data-aos="fade-up">
+            <div className=' p-[0.5rem] text-justify rounded-xl text-[18px] items-center mt-6'>
+              Tulasee is a highly intelligent and competent programmer/developer. He is able to work in complex and results-driven cultures and his attention to each and every
+              detail in a development project is brilliant. Tulasee has a knack for investigating and identifying.....
+              <span className='text-[#BEF6A9]'> more</span>
+            </div>
+
+            <div className='mb-2 px-2 py-2 mt-4 text-white rounded-xl flex items-center'>
+              <Image src={paul_brobson} alt='paul_brobson img' className='rounded-full m-2 w-[70px] h-[70px]' />
+              <div className='flex flex-col ml-2 text-left'>
+                <span className='font-sf_pro_display text-[16px]'>Paul Brobson</span>
+                <span className='font-sf_pro_display text-[20px]'>Founder, Trusted Sale, Inc.</span>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+        {/* image with quote */}
+        <div className="flex w-fit h-[80vh] lg:w-[1130px] lg:h-[832px] md:bg-cover items-center justify-center rounded-2xl text-white p-4"
+          data-aos="fade-up"
+          style={{
+            backgroundImage: "url('/mountain.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}>
+          <div className='flex flex-col'>
+            <div className='flex items-center m-4'>
+              <Image src={name_img} alt="name_img" className="rounded-full w-[200px] h-[100px]" />
+            </div>
+            <div className='mt-8 lg:mt-28'>
+              <p className="text-[14px] lg:text-[25px] font-bold w-1/2 font-pockota mb-2 justify-center">
+                Stay busy building tools for users. Don&apos;t be a boilerplate programmer.
+                Instead, build tools for users and other programmers.
+                Take historical note of textile and steel industries:
+                do you want to build machines and tools, or do you want to operate those machines.
+              </p>
+              <p className="text-[14px] lg:text-[20px] font-pockota mb-2 item-end">
+                — Ras Bodik at the start of his compilers course
+              </p>
+            </div>
+            <div className='mt-2 lg:mt-12'>
+              <p className="text-center font-inter font-bold text-[14px] lg:text-[20px]">
+                The top of one mountain is always the bottom of another.
+              </p>
+              <p className="text-center font-inter text-[14px] lg:text-[15px]">
+                — Marianne Williamson
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className='grid grid-cols-3 items-center justify-center md:mx-[17%] md:my-[4%] gap-4 py-[1rem] md:pl-[10%]
+        text-[15px] text-white border-t border-gray-200'>
+
+        <Link href='https://twitter.com/tulasirao'>
+          <div className='flex ml-[2rem]'>
+            <Image src={X_logo} alt='tulasee_X' className='text-[#989296] relative w-[17px] h-[17px]' />
+            <span className='ml-2'>Twitter</span>
+          </div>
+        </Link>
+
+        <Link href='https://www.linkedin.com/in/tulaseeraochintha'>
+          <div className='flex'>
+            <LinkedInIcon className='text-[#989296]' />
+            <span className='ml-2'>Linkedin</span>
+          </div>
+        </Link>
+
+        <Link href='https://github.com/tulaseerao'>
+          <div className='flex'>
+            <GitHubIcon className='text-[#989296]' />
+            <span className='ml-2'>GitHub</span>
+          </div>
+        </Link>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default CaseStudies;
---
+++ src/app/page.js
@@ -0,0 +1,158 @@
+'use client'
+import React, { useEffect } from "react";
+import CaseStudies from "./components/CaseStudies";
+import Image from 'next/image';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import sir from '../../public/tulasee_sir_img.jpg';
+import logo from '../../public/yc_logo.png';
+import img from '../../public/645.png';
+import atl from '../../public/atl.png';
+import qlarant from '../../public/qlarant.png';
+
+export default function Home() {
+
+  useEffect(() => {
+    AOS.init({
+      delay: 0,
+      duration: 400,
+      easing: "ease",
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#151718] cursor-default">
+
+      <div className="flex flex-col w-full items-center justify-center text-white p-[55px] sm:p-[75px]"
+        style={{
+          backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/bg_img.png')"
+        }}>
+        <Image src={sir} alt="img" className="rounded-full h-[90px] w-[90px] bg-slate-800" data-aos="fade-in" />
+        <p className="font-pockota md:text-[32px] text-[24px] sm:text-[28px] my-2" data-aos="fade-out">Tulasee Rao Chintha</p>
+        <p className="text-[20px] font-pockota mb-[20px]" data-aos="fade-in">
+          <span>
+            Co-founder and CTO of InpharmD
+          </span>
+          <span>  | </span>
+          <span>
+            YC W21
+          </span>
+        </p>
+        <p className="text-center font-inter text-[14px] md:text-[20px] md:w-[70%]" data-aos="fade-up">
+          A real AI Specialist (wrote ~10k lines of AI code) | Raised $12 millions | AI Pending Patent | LLM&apos;s Expert - Fine-tuned a 40-billion-parameter open-source LLM.
+        </p>
+      </div>
+
+      <div className="bg-[#0003] p-4 flex items-center justify-center ">
+        <div className="flex flex-col text-white w-full md:w-[60%] 2xl:max-w-[1280] justify-center items-center">
+          <p className="text-[28px] md:text-[32px] font-pockota my-[2rem] text-center" data-aos="fade-in">My Journey </p>
+
+          <div data-aos="fade-up" className="w-[80%] md:w-full">
+            <p className="text-[20px] md:text-[22px] font-sf_pro_display font-semibold my-2 ">
+              👨‍🌾 Farmer to Pharma CTO
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] text-[] font-inter my-4">
+              My journey began in a tiny sweet green little village in Southern India, aiming to farm. But life steered me towards engineering, and eventually, I became the CTO at InpharmD. Grateful for this unexpected path that led me to where I am today.
+            </p>
+            <p className="text-[14px] md:text-[18px] font-inter my-4">
+              My journey spans tech roles in India and Corporate America, leading to co-founding InpharmD.
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] font-inter my-4">
+              Built an AI-driven platform that transforms healthcare information management, offering precise responses to healthcare professionals. With evidence-based insights, we&apos;re making waves in the sector by fostering innovation and driving efficiency like never before, providing them with succinct summaries of literature.
+            </p>
+          </div>
+
+          <div data-aos="fade-up" className="w-[80%] md:w-full">
+            <p className="text-[20px] md:text-[22px] font-sf_pro_display font-semibold my-2 ">
+              💰Humble beginnings from zeros to billions
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] font-inter my-4">
+              In 2018, I co-founded InpharmD. With seed funding of $8.05M, we quickly reached series A, boasting a revenue between $4-8M. We now have a vibrant community of over 50k users, including 10k active daily users.
+            </p>
+          </div>
+
+          <div className="mb-2 w-[80%] md:w-full" data-aos="fade-up">
+            <p className="text-[20px] md:text-[22px] font-sf_pro_display font-semibold my-2 ">
+              👨‍💼 Investor onboard
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] font-inter my-4">
+              Our recent funding round was backed by some of the most prestigious investors - 645 Ventures, Atlanta Ventures, Y Combinator, & Qlarant Capital
+            </p>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-4 my-4 items-center justify-center gap-4 xl:gap-[4rem] rounded-xl
+           bg-gradient-to-br from-[#1d1c1d] 
+          to-[#353536] bg-opacity-50 py-[2rem] px-[2rem] md:px-[4rem] w-full" data-aos="fade-up">
+
+            <a href="https://www.ycombinator.com/companies/inpharmd" target="_blank" className='flex items-center justify-center border-[2px] border-gray-700 p-4 xl:p-[2rem] rounded-xl
+            bg-gradient-to-b from-[#252427] to-[#424242] shadow-xl cursor-pointer'>
+              <Image src={logo} alt="img" className="rounded-full w-[50px] h-[50px] bg-slate-800" />
+            </a>
+
+            <a href="https://645ventures.com/homepage-databases/inpharmd" target="_blank" className='flex items-center justify-center border-[2px] border-gray-700 p-4 xl:p-[2rem] rounded-xl
+            bg-gradient-to-b from-[#252427] to-[#424242] shadow-xl cursor-pointer'>
+              <Image src={img} alt="img" className="rounded-full w-[50px] h-[50px] bg-slate-800" />
+            </a>
+
+            <a href="https://www.atlantaventures.com/news/why-we-invested-in-inpharmd" target="_blank" className='flex items-center justify-center border-[2px] border-gray-700 p-4 xl:p-[2rem] rounded-xl
+            bg-gradient-to-b from-[#252427] to-[#424242] shadow-xl cursor-pointer'>
+              <Image src={atl} alt="img" className="rounded-full w-[50px] h-[50px] bg-slate-800" />
+            </a>
+
+            <a href="https://www.qlarant.com/about/news/qlarant-capital-becomes-investor-in-inpharmd/" target="_blank" className='flex items-center justify-center border-[2px] border-gray-700 p-4 xl:p-[2rem] rounded-xl
+            bg-gradient-to-b from-[#252427] to-[#424242] shadow-xl cursor-pointer'>
+              <Image src={qlarant} alt="img" className="rounded-full w-[50px] h-[50px] bg-slate-800" />
+            </a>
+
+          </div>
+
+          <div className="mb-[3rem] w-[80%] md:w-full" data-aos="fade-up">
+            <p className="text-[20px] md:text-[22px] font-sf_pro_display font-semibold my-6 ">
+              👨‍💼 InpharmD CTO
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] font-inter my-4">
+              As CTO and Co-founder of InpharmD, I drive technology development, harnessing AI for enhanced patient care and healthcare provider support. I lead web and AI initiatives, processing 30M medical papers, shaping industry advancements.
+            </p>
+            <p className="text-[14px] text-justify md:text-[18px] font-inter mb-6">
+              My focus spans technical strategy, team leadership, and product delivery, ensuring top-notch solutions. Committed to healthcare and AI evolution, I aim to sustain impactful innovation. Enthusiastic about our team and InpharmD&apos;s vision, I&apos;m eager to propel positive change in healthcare through cutting-edge approaches.
+            </p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full items-center justify-center text-white p-[55px] sm:p-[75px]"
+        style={{
+          backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/bg_img.png')"
+        }}>
+        <div className="flex flex-col w-full md:w-[60%] items-center justify-center">
+          <span className="text-[28px] md:text-[32px] font-pockota my-[2rem]" data-aos="fade-in">About InpharmD</span>
+          <p className="text-[14px] text-justify md:text-[20px] font-inter my-[2rem]" data-aos="fade-up">
+            <span className="text-orange-600"> InpharmD </span>  answers complex questions about drugs by using a combination of generative AI and a clinical pharmacist. Clinicians can ask questions about whether a drug is right to give a patient, dosage and side effect questions, and what to do if a drug is not on hand. Hospital committees (P&T) that decide which drugs to use can generate reports on cost-effectiveness of drugs.
+          </p>
+
+          <p className="text-[14px] text-justify md:text-[20px] font-inter mb-[2rem]" data-aos="fade-up">
+            “My co-founder Tulasee led the way in AI advancements, helping us transcribe PDFs faster than our pharmacists, though still refining accuracy. With 5,000 study abstracts as our foundation, we&apos;ve fine-tuned algorithms to match our abstracts. Testing at 94% against humans, we aim for 100% accuracy in medical data reliability. Introducing Sherlock, our answer to missed opportunities like Watson. Partnering with ASHP, we&apos;re integrating 1,300 vetted drug monographs, enabling Sherlock to provide precise answers at the point of care. Excited to revolutionize healthcare decisions with seamless AI integration”
+          </p>
+
+          {/* Video asset is unavailable in the current public/ directory; keep the section without a broken media reference */}
+          <div className="flex items-center bg-[rgb(227,247,248)] rounded-xl justify-center mx-auto my-[2rem] px-6 py-10" data-aos="fade-out">
+            <p className="text-center font-inter text-[14px] md:text-[18px] text-black">
+              InpharmD product demo preview
+            </p>
+          </div>
+
+          <button className="font-sf_pro_display border-[4px] font-medium text-[12px] lg:text-[24px] bg-white text-black rounded-xl px-4 py-2 shadow-xl"
+            data-aos="fade-in" >
+            <a href="https://news.ycombinator.com/item?id=25957775" target="_blank">
+              Patented AI Algorithm
+            </a>
+          </button>
+        </div>
+      </div>
+
+      <CaseStudies />
+    </div>
+
+  );
+}
